4a6297b67b3bed66f88cd31e5d7cb2e607185f84
Shyness <109921160+abubakar0325@users.noreply.github.com>
2025-02-14 23:44:56 +0100

    Update todo.js
    All done about storage

--- todo.js
+++ todo.js
@@ -1,54 +1,58 @@
+// Initialize array from localStorage or create a default array
+let myArray2 = JSON.parse(localStorage.getItem('myStorage')) || [{
+  inputValue: "Sleeping",
+  dateValue: 'djdidj'
+}];
 
-// creating array for storing data from Imputs
-let myArray2 = JSON.parse(localStorage.getItem('myStorage')); 
-if (!myArray2){
-  myArray2 = [{
-    inputValue: "Sleeping",
-    dateValue: 'djdidj'}]};
-
-function storageFun(){
-localStorage.setItem('myStorage', JSON.stringify(myArray2));
+// Function to save data to localStorage
+function storageFun() {
+  localStorage.setItem('myStorage', JSON.stringify(myArray2));
 }
-//function for collecting data from inputs and put it in the array
-document.querySelector('.add-button')
-  .addEventListener('click', () => {
-    let todoInput = document.getElementById('todo-input').value;
-    let dateinput = document.querySelector('.date-input').value;
-    myArray2.push({
-      inputValue: todoInput,
-      dateValue: dateinput,
-    });
-    //clear input after collecting data
-    storageFun();
-    document.getElementById('todo-input').value = '';
-    document.querySelector('.date-input').value = '';
-    generateHTML();
-  })
-  //function for generating HTMl by using our array data and display it in the page
-  function generateHTML(){
-  let valueToDisplay = '';
-  //loop through the array
-  myArray2.forEach((element, index) => {
-    valueToDisplay+= `
-      <p class="todo-name">${element.inputValue}</p>
-      <p class="todo-date">${element.dateValue}</p>
-      <button class="todo-button" onclick="deleteItem(${index})">Delete</button>
-    `;
+
+// Add event listener to the "Add" button
+document.querySelector('.add-button').addEventListener('click', () => {
+  let todoInput = document.getElementById('todo-input').value;
+  let dateinput = document.querySelector('.date-input').value;
+  myArray2.push({
+    inputValue: todoInput,
+    dateValue: dateinput,
   });
-  
-  //put the HTML in the page
-  document.querySelector('.generated-html').innerHTML = valueToDisplay;
-}
-
-function deleteItem(index) {
-  myArray2.splice(index, 1);
-  generateHTML();
+  // Save to localStorage and clear inputs
   storageFun();
-  
-};
-
-
-
-document.getElementById('todo-input').addEventListener("input", () => {
+  document.getElementById('todo-input').value = '';
+  document.querySelector('.date-input').value = '';
   generateHTML();
 });
+
+// Function to generate HTML and display tasks
+function generateHTML() {
+  let valueToDisplay = '';
+  myArray2.forEach((element, index) => {
+    valueToDisplay += `
+      <div class="todo-item">
+        <p class="todo-name">${element.inputValue}</p>
+        <p class="todo-date">${element.dateValue}</p>
+        <button class="todo-button" data-index="${index}">Delete</button>
+      </div>
+    `;
+  });
+  // Insert HTML into the page
+  document.querySelector('.generated-html').innerHTML = valueToDisplay;
+
+  // Add event listeners to the delete buttons
+  document.querySelectorAll('.todo-button').forEach(button => {
+    button.addEventListener('click', () => {
+      deleteItem(button.getAttribute('data-index'));
+    });
+  });
+}
+
+// Function to delete an item
+function deleteItem(index) {
+  myArray2.splice(index, 1);
+  storageFun();
+  generateHTML();
+}
+
+// Initial render of tasks
+generateHTML();
